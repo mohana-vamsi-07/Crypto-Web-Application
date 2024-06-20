4db34de45bdac6f92170d2e5b3202d3003d4ed55
unknown <mohanavamsi02@gmail.com>
2024-06-20 10:43:06 +0530

    first commit

--- src/Components/ImgCard.js
+++ src/Components/ImgCard.js
@@ -3,9 +3,8 @@ import React from 'react'
 const ImgCard = (props) => {
   return (
     <>
-    <img src={props.url} alt={props.name} width={50} height={50}/>
+    <img src={props.url} alt={props.name} width={50} height={50}/><hr/>
     <p style={{color:"orange",fontWeight:"bold"}}>{props.name}</p>
-    
     <p>Rank : {props.rank}</p>
     <p>Symbol : {props.symbol}</p>
     <p>Price in $ : {props.price}</p>
